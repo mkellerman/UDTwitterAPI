6a73d9d3077c1f5c4b4a429ddd8a5e8ff083a608
Marc R Kellerman <9411143+mkellerman@users.noreply.github.com>
2020-05-04 10:42:33 -0700

    Added UDFooter

--- dashboard.ps1
+++ dashboard.ps1
@@ -7,6 +7,7 @@ $global:DataPath = New-Item -Path './Data' -ItemType Directory -Force | Select-O
 
 $UDPages = Get-ChildItem -Path './Pages/*.ps1' | ForEach-Object { . $_.FullName }
 $UDTheme = Get-UDTheme -Name DefaultTight
+$UDFooter = New-UDFooter -Links (New-UDLink -Text '@mkellerman (GitHub)' -Url 'https://github.com/mkellerman')
 
-$Dashboard = New-UDDashboard -Title "PSTwitterAPI Dashboard" -Pages $UDPages -Theme $UDTheme
+$Dashboard = New-UDDashboard -Title "PSTwitterAPI Dashboard" -Pages $UDPages -Theme $UDTheme -Footer $UDFooter
 Start-UDDashboard -Dashboard $Dashboard -Port 10001 -Force
